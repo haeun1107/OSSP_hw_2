d835cff491f7604c76b43b8240a551d9b659ffe9
haeun1107 <haeun9394@gmail.com>
2024-03-22 10:57:10 +0900

    2024-03-20 OSSP 개인과제#2

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string name;
+    int num;
+
+    cout << "이름을 입력하세요 : ";
+    cin >> name;
+
+    cout << "학번을 입력하세요 : ";
+    cin >> num;
+
+    cout << endl;
+
+    cout << "<출력>" << endl;
+    cout << "이름 : " << name << endl;
+    cout << "학번 : " << num << endl;
+}
